4c00e078e491e5206751c4f35e02ae7e8a407de4
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-25 04:32:16 +0900

    modelとview作成(sessionsやpasswordはコメントアウト)

--- musubi/app/controllers/users_controller.rb
+++ musubi/app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
-  
+
   def index
     @users = User.paginate(page:params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    @receiver_id = Thanksletter.where(receiver_id: @user.id)
-    @thanksletters = @receiver_id.paginate(page: params[:page])
+    # @receiver_id = Thanksletter.where(receiver_id: @user.id)
+    # @thanksletters = @receiver_id.paginate(page: params[:page])
   end
 
   def new
