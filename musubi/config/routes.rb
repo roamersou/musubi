f43f8e99968698650b460fd423dd172289e108fc
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-25 13:29:15 +0900

    controllerとフォームの作成

--- musubi/config/routes.rb
+++ musubi/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
-  # root 'application#hello'
   root "home#index"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+  # root 'application#hello'
   resources :users
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
