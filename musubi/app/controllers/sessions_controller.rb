28daf8606986a52f05f442525554ff3519eda3d3
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-25 14:21:13 +0900

    sessionのcreateメソッドとlog_inメソッドの作成

--- musubi/app/controllers/sessions_controller.rb
+++ musubi/app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      redirect_to user
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash.now[:danger] = "メールアドレスかパスワードが間違ってるよ！"
+      render "new"
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to("/")
+  end
+  
 end
