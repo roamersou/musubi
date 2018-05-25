c7476d1eff3457c7c4c4658df7bd3d99213196ae
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-25 14:56:45 +0900

    より長いログイン機構の実装

--- musubi/app/controllers/sessions_controller.rb
+++ musubi/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      # params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
-    redirect_to("/")
+    redirect_to root_url
   end
-  
+
 end
