796167ba439afd2616fbc491ef500cc5d04003a7
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-27 00:40:15 +0900

    remember_meのチェックボックス追加

--- musubi/app/controllers/sessions_controller.rb
+++ musubi/app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      # params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      # ログイン後にユーザー情報のページにリダイレクトする
     else
       flash.now[:danger] = "メールアドレスかパスワードが間違ってるよ！"
       render "new"
