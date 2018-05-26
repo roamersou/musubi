01c1f96ab51e683ce1997748659a28db4225517f
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-26 13:06:11 +0900

    永続cookiesを作成

--- musubi/app/helpers/sessions_helper.rb
+++ musubi/app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
    # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
