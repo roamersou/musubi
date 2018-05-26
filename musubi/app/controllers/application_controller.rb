31cd3e9392473e213fccf9516f28140775c2360e
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-27 01:32:15 +0900

    logged_in_userメソッドの定義

--- musubi/app/controllers/application_controller.rb
+++ musubi/app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::Base
 
   private
 
-   # ユーザーのログインを確認する
-    # def logged_in_user
-    #   unless logged_in?
-    #     store_location
-    #     flash[:danger] = "ログインしてください"
-    #     redirect_to login_url
-    #   end
-    # end
+  #  ユーザーのログインを確認する
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
 end
