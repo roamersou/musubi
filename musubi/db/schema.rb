96648323e0063a2e3c956c2378ae787b7e5669a7
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-27 10:35:05 +0900

    管理ユーザーの定義とカラム追加

--- musubi/db/schema.rb
+++ musubi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180525070346) do
+ActiveRecord::Schema.define(version: 20180527011717) do
 
   create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180525070346) do
     t.string "insta_account"
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
