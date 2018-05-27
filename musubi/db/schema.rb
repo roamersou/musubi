a95c0fcbabc9a73919e1f0b6ed12dbce424163bb
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-28 04:50:23 +0900

    receiver_idカラムの追加

--- musubi/db/schema.rb
+++ musubi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180527015015) do
+ActiveRecord::Schema.define(version: 20180527194849) do
 
   create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180527015015) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "receiver_id"
     t.index ["user_id", "created_at"], name: "index_thanksletters_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_thanksletters_on_user_id"
   end
