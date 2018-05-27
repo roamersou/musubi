8e88376827ac31c557693692ddd4cf50733857f5
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-28 06:04:13 +0900

    routes, controller, viewの作成

--- musubi/config/routes.rb
+++ musubi/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'application#hello'
   resources :users
   resources :events
+  resources :thanksletters, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
