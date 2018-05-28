c7488fa56ed1b902c31e04ca3c926d6271a4baad
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-28 12:26:54 +0900

    pay_forwardモデルの定義とplaceカラム追加

---
+++ musubi/app/models/pay_forward.rb
@@ -0,0 +1,3 @@
+class PayForward < ApplicationRecord
+    belongs_to :user
+end
---
+++ musubi/test/models/pay_forward_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PayForwardTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
