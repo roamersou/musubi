736eaf87383333c743825819156a2f6f27be6d98
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-05-31 19:39:34 +0900

    high_voltage追加と静的ページの作成

--- musubi/config/routes.rb
+++ musubi/config/routes.rb
@@ -8,6 +8,4 @@ Rails.application.routes.draw do
   resources :payforwards
   resources :events
   resources :thanksletters, only: [:new, :create, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end
