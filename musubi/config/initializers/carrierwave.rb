686e8270f59e145db23935f8f74f91a1b5ad51e8
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-06-29 17:05:24 +0900

    carrierwave内に環境変数を宣言

--- musubi/config/initializers/carrierwave.rb
+++ musubi/config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
       config.fog_credentials = {
         # Amazon S3用の設定
         :provider              => 'AWS',
-        :region                => ENV['ap-northeaset-1'],     # 例: 'ap-northeast-1'
-        :aws_access_key_id     => ENV['AKIAIOQMZ2WLJEAH7GNQ'],
-        :aws_secret_access_key => ENV['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']
+        :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY']
       }
-      config.fog_directory     =  ENV['musubi-thanks']
+      config.fog_directory     =  ENV['S3_BUCKET']
     end
 end
