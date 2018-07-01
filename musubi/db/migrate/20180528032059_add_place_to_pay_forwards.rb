9f3b9fcf95b700d9467de1f3937a0499e7ac6d21
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-07-01 14:16:49 +0900

    pay_forwardマイグレーションファイルの削除

--- musubi/db/migrate/20180528032059_add_place_to_pay_forwards.rb
+++
@@ -1,5 +0,0 @@
-class AddPlaceToPayForwards < ActiveRecord::Migration[5.1]
-  def change
-    add_column :pay_forwards, :place, :string
-  end
-end
--- musubi/db/migrate/20180530043430_drop_table_pay_forwards.rb
+++
@@ -1,5 +0,0 @@
-class DropTablePayForwards < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :pay_forwards
-  end
-end
