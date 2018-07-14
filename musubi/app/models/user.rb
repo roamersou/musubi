1227794a591aeae1af81117c2201d08e537bf043
Ryoma Tokushige <ryoma.tokushige@gmail.com>
2018-07-14 22:09:38 +0900

    自己紹介を150文字に増量

--- musubi/app/models/user.rb
+++ musubi/app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
                uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length:{minimum:6}, allow_nil: true
-    validates :self_intro, length:{maximum: 100}
+    validates :self_intro, length:{maximum: 150}
     validate :picture_size
 
     # fixture向けのdigestメソッド追加
