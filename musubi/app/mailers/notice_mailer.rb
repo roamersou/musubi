b11c983e49d9c67ea73a8eb2166003085947a4c7
Ryuchell <roamers.yano@gmail.com>
2019-02-21 20:57:25 +0900

    メール送信者Musubiに設定

--- musubi/app/mailers/notice_mailer.rb
+++ musubi/app/mailers/notice_mailer.rb
@@ -1,7 +1,6 @@
 class NoticeMailer < ActionMailer::Base
-  default from: ENV['USER_NAME'],
+  default from: '"Musubi" <ENV['USER_NAME']>',
   bcc: "roamers.yano@gmail.com"
- 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
