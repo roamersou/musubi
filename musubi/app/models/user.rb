cfad07edd8e2c8388f8a5e0c38992e0cf013dfbf
Ryuchell <roamers.yano@gmail.com>
2019-02-21 20:42:55 +0900

    自己紹介文字数エラー解消

--- musubi/app/models/user.rb
+++ musubi/app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
                uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length:{minimum:6}, allow_nil: true
-    validates :self_intro, length:{maximum: 150}
+    # validates :self_intro, length:{maximum: 150}
     validate :picture_size
 
     # fixture向けのdigestメソッド追加
